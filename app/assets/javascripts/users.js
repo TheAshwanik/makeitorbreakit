3829dcd34f39bda398a9a745b0213fa1bc4ac580
sarah niemiec <sroseniemiec@gmail.com>
2017-05-30 00:16:41 -0400

    more ujs

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,15 +5,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
   var about = document.getElementById("about")
   var aboutText = document.getElementById("abouttext")
   var close = document.getElementById("close")
+  var modalOverlay = document.querySelector(".modaloverlay")
 
   aboutText.style.display = "none"
+  modalOverlay.style.display = "none"
 
   about.addEventListener("click", function () {
     aboutText.style.display = "block"
+    modalOverlay.style.display = "block"
   })
 
   close.addEventListener("click", function () {
     aboutText.style.display = "none"
+    modalOverlay.style.display = "none"
   })
 
 
